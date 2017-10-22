3fc51e6d3feef674435c34ff28f61af1c50efa71
Michael <mnpa1984@gmail.com>
2017-10-22 15:06:13 -0400

    Âncora animada

--- js/main.js
+++ js/main.js
@@ -3,5 +3,8 @@ $("#slider").owlCarousel({
       paginationSpeed : 400,
       autoPlay : 5000,
       singleItem:true
-      
 });
+$("ul.navbar-nav > li > a").on('click', function(event){
+	event.preventDefault();
+	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+})
